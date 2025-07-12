819d10e4327094eae4bab2900ac1a0f437fc5ad3
abhishek shah <shahabhishek456@gmail.com>
2025-07-12 20:00:11 +0545

    deploye error found and debuged

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Task1 from './components/task1/Task1.jsx'
-import Task2 from "./components/task2/task2.jsx"
+import Task2 from "./components/task2/Task2.jsx"
 import Task3 from "./components/task3/Task3.jsx"
 
 const App = () => {
